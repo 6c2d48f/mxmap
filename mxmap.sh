e6d227b0e5c9ffdde99b74aac684d40ba9c74ee5
6c2d48f <89391580+6c2d48f@users.noreply.github.com>
2021-08-28 10:43:49 +0800

    Update mxmap.sh

--- mxmap.sh
+++ mxmap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-count=0
-if [[ "$1" =~ ^[a-zA-Z0-9.]*[a-zA-Z0-9]{0,62}$ ]];then
-	for line in $(dig -t mx +short $1)
+count=0 
+if [[ "${@: -1}" =~ ^([a-zA-Z0-9.]*\.)?[a-zA-Z0-9]{1,62}$ ]];then
+	for line in $(dig -t mx +short ${@: -1})
 	do
 		if ((count%2));then
 			servers[$((count/2+1))]=${line%.*}
@@ -9,11 +9,16 @@ if [[ "$1" =~ ^[a-zA-Z0-9.]*[a-zA-Z0-9]{0,62}$ ]];then
 		count=$((count+1))
 	done
 	echo "Query to the post office server domain name:  ${servers[*]}"
-	nmap ${servers[*]} ${*:2:$#}
+	if [[ "${*:1:$#-1}" != "--noscan" ]];then
+		nmap ${servers[*]} ${*:1:$#-1}
+	fi
 elif [[ "$1" =~ ^(|-(h|-help))$ ]];then
-	echo "Usage:  mxmap domain [Options]"
+	echo "Usage:  mxmap [Options] domain"
 	echo "Where:  domain	  is in the Domain Name System"
+	echo "Options:"
+	echo "	--noscan	No scanning of post office servers"
 	echo "Use \"nmap --help\" for complete list of options"
 else
 	echo "Error: Illegal parameters!"
+	exit 1
 fi
